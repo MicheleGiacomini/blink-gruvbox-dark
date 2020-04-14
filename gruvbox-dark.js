35fb7a32f2c35425cf7cbb6011a2ff644f6630c7
MicheleGiacomini <39375954+MicheleGiacomini@users.noreply.github.com>
2020-04-14 10:08:29 +0100

    Update gruvbox-dark.js

--- gruvbox-dark.js
+++ gruvbox-dark.js
@@ -23,4 +23,4 @@ t.prefs_.set('color-palette-overrides',
 
 t.prefs_.set('cursor-color', 'rgba(208, 208, 208, 0.5)');
 t.prefs_.set('foreground-color', lightWhite);
-t.prefs_.set('backgroud-color', black);
+t.prefs_.set('background-color', black);
